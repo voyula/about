ed339540dcf97ddae0a5caaa7480d2453e5abff5
Voyula <mail43654@mail.com>
2019-06-06 10:11:41 +0300

    Update

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ import Header from '../components/header'
 export default () => (
   <main>
     <Header />
-  	<p>Email: jesus@example.com</p>
+    <p>Email: jesus@example.com</p>
     <section>
       <Link href="/">
         <a>Go to Home</a>
